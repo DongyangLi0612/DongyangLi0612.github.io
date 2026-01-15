cc5dc2b1c70cc560dde06f8e3a736807d949a145
DongyangLi0612 <fromdongyang@gmail.com>
2026-01-15 17:25:39 +0800

    Update scripts.js

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
   }
 
   if (lang === "zh") {
-    document.getElementById("content_wrapper").classList.add("rtl_wrapper");
+    document.getElementById("content_wrapper").classList.add("ltr_wrapper");
     if (
       document.getElementById("languageIcon") &&
       document.getElementById("languageText")
@@ -18,7 +18,7 @@ $(document).ready(function () {
       document.getElementById("languageText").innerText = "EN";
     }
   } else {
-    document.getElementById("content_wrapper").classList.add("rtl_wrapper");
+    document.getElementById("content_wrapper").classList.add("ltr_wrapper");
     if (
       document.getElementById("languageIcon") &&
       document.getElementById("languageText")
