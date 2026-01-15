336341491b4ac0a15630b713627dfe0f67808376
DongyangLi0612 <fromdongyang@gmail.com>
2026-01-15 16:37:18 +0800

    Create zh.js

---
+++ lang/zh.js
@@ -0,0 +1,158 @@
+// ---------- Navbar titles
+const enNavbarData = {
+  title: " 李东阳",
+  Home: "主页",
+  publications: "学术成果",
+  Research: "科研项目",
+  Jobs: "工作经历",
+  Contact: "联系方式",
+};
+
+// ---------- Home page data
+const enHomePageData = {
+  name: " 李东阳",
+  jobTitle: " 讲师 ",
+  home_title: "About me",
+  home_content: `
+  <div>
+    <p> 我是上海电力大学人工智能学院的一名讲师。我在华东师范大学（ECNU）获得博士学位，导师为何晓丰教授。
+目前，我的研究兴趣包括大语言模型、多模态大语言模型以及多智能体系统。
+我已在国际顶级会议和期刊上发表了20余篇论文，包括 ACL、AAAI、EMNLP、COLING、KBS、ECAI 等。
+    </p>
+    <h2 class='title'> Research Interests </h2>
+    <p>招收具有硕士研究生，如有兴趣欢迎联系沟通！ </p>
+    <ul>
+      <li> 大模型（文本模态、多模态）</li>
+      <li> 多智能体</li>
+      <li> 知识增强的语言模型 </li>
+    </ul>
+  </div>
+  `,
+};
+
+// ---------- Publications page data
+const enPublicationsPageData = {
+  type_one_title: "Journal Papers",
+
+  type_one_items: [
+    {
+      title: `Dancing to the beat of climate change: Studying visual frames on TikTok through the left-right divide among U.S. politicians (with ).`,
+      abstract: `Politicians across the political spectrum have taken to TikTok in an attempt to reach young voters by producing original videos. This novel trend has turned TikTok into the new polarizing ring in the political communication battle, deepening the partisan breach around issues such as climate change. However`,
+      date: "",
+      link: "http://link.com",
+      github: "",
+      download: "",
+      writers: ["Claudio Cioffi-Revilla", "masoud Claudio "],
+    },
+    {
+      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
+      abstract: `Politicians across the political spectrum have taken to TikTok in an attempt to reach young voters by producing original videos. This novel trend has turned TikTok into the new polarizing ring in the political communication battle, deepening the partisan breach around issues such as climate change. However`,
+      date: "1996",
+      link: "",
+      github: "http://github.com",
+      writers: ["Claudio Cioffi-Revilla", ""],
+    },
+  ],
+
+  type_two_title: "Under Review",
+  type_two_items: [
+    {
+      title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
+      abstract: ``,
+      date: "1996",
+      link: "",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
+      abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
+      date: "1996",
+      link: "http://link.com",
+      github: "http://github.com",
+      writers: ["Claudio Cioffi-Revilla", "masoud Claudio "],
+    },
+  ],
+
+  type_three_title: "Book Chapters",
+  type_three_items: [
+    {
+      title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
+      abstract: ``,
+      date: "",
+      link: "",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
+      abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
+      date: "1996",
+      link: "http://link.com",
+      github: "http://github.com",
+      writers: ["Claudio Cioffi-Revilla"],
+    },
+  ],
+
+  type_four_title: "",
+  type_four_items: [
+    {
+      title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
+      abstract: ``,
+      date: "",
+      link: "",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
+      abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
+      date: "1996",
+      link: "http://link.com",
+      github: "http://github.com",
+      writers: ["Claudio Cioffi-Revilla"],
+    },
+  ],
+};
+
+// ---------- Research page data
+const enResearchPageData = {
+  title: "Research",
+  content: `
+    <div class='research_content'>
+        <p> My research goal is to combine the three major sub-fields of the Computational Social Science, including Social Media Data Analysis, Network Analysis, and Agent-based Modeling, to study various social science problems. Below you can find a summary of my past and current projects.</p>
+        <br />
+        <h4 style="font-size: 1.1rem "> Identifying and Characterizing US Domestic Ideological Extremists </h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+        <img src='../files/images/research.png' />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra  accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat <br /> imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet <br /> lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+    </div>
+  `,
+};
+
+// ---------- Jobs page data
+const enJobsPageData = {
+  title: "Jobs",
+  items: [
+    {
+      title: `Job title`,
+      company: "Company name",
+      startData: "20 April 2019",
+      endDate: "20 April 2022",
+      location: "Berlin",
+      abstract:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus.",
+      achievements: ["achievement 1", "achievement 2", "achievement 3"],
+    },
+    {
+      title: `Job title`,
+      company: "Company name",
+      startData: "20 April 2019",
+      endDate: "",
+      location: "Berlin",
+      abstract:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus.",
+      achievements: ["achievement 1", "achievement 2"],
+    },
+  ],
+};
