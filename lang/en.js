e36bef7cb63529a02c1b11e51b8c16834c1160b5
DongyangLi0612 <fromdongyang@gmail.com>
2026-01-17 15:56:13 +0800

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -277,20 +277,20 @@ const enPublicationsPageData = {
   //   },
   // ],
 
-  // type_two_title: `"Research Projects"`,
-  // type_two_items: [
-  //   {
-  //     title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
-  //     date: "1996",
-  //     writers: [],
-  //   },
-  //   {
-  //     title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
-  //     abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
-  //     date: "1996",
-  //     writers: ["Claudio Cioffi-Revilla", "masoud Claudio "],
-  //   },
-  // ],
+  type_two_title: `"Research Projects"`,
+  type_two_items: [
+    {
+      title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
+      date: "1996",
+      writers: [],
+    },
+    {
+      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
+      abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
+      date: "1996",
+      writers: ["Claudio Cioffi-Revilla", "masoud Claudio "],
+    },
+  ],
 
   // type_three_title: "Book Chapters",
   // type_three_items: [
