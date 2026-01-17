59f152ca1de9fb7a092d7f414a8a12f5d6d2606b
DongyangLi0612 <fromdongyang@gmail.com>
2026-01-17 19:22:35 +0800

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -173,42 +173,7 @@ const enPublicationsPageData = {
       <strong>Dongyang Li</strong>, Taolin Zhang, Jiali Deng, Longtao Huang, Chengyu Wang, Xiaofeng He, Hui Xue<br>
       <strong>COLING 2024 (CCF-B)</strong>
       </li>
-      <li> <strong>MCFC: A Momentum-Driven Clicked Feature Compressed Pre-trained Language Model for Information Retrieval</strong><br>
-      <strong>Dongyang Li</strong>, Ruixue Ding, Pengjun Xie, Xiaofeng He<br>
-      <strong>NLPCC 2024 (CCF-C)</strong>
-      </li>
-      <li> <strong>GeoGLUE: A Chinese GeoGraphic Language Understanding Evaluation Benchmark</strong><br>
-      <strong>Dongyang Li</strong>, Ruixue Ding, Qiang Zhang, Zheng Li, Boli Chen, Pengjun Xie, Yao Xu, Xin Li, Ning Guo, Fei Huang, Xiaofeng He<br>
-      <strong>ADMA 2024 (CCF-C)</strong>
-      </li>
-      <li> <strong>DAFNet: Dynamic Auxiliary Fusion for Sequential Model Editing in Large Language Models</strong><br>
-      Taolin Zhang, Qizhou Chen, <strong>Dongyang Li (Co-first author)</strong>, Chengyu Wang, Xiaofeng He, Longtao Huang, Hui Xue, Jun Huang<br>
-      <strong>ACL 2024 (CCF-A)</strong>
-      </li>
-      <li> <strong>R4: Reinforced Retriever-Reorder-Responder for   Retrieval-Augmented Large Language Models</strong><br>
-      Taolin Zhang, <strong>Dongyang Li (Co-first author)</strong>, Qizhou Chen, Chengyu Wang, Longtao Huang, Hui Xue, Xiaofeng He, Jun Huang<br>
-      <strong>ECAI 2024 (CCF-B)</strong>
-      </li>
-      <li> <strong>Text-Driven Hybrid Curriculum Learning for Multimodal Sentiment Analysis</strong><br>
-      Feifei Xu, Puzhe Li, <strong>Dongyang Li<sup>✉</sup><strong>, Luobin Huang, and Wenjing Zhu<br>
-      <strong>MMM 2026 (CCF-C)</strong> <br>
-      </li>
-      <li> <strong>Question-Aware Spatial-Temporal Reasoning in Patch for Audio-Visual Question Answering</strong><br>
-      Feifei Xu, Wenjing Zhu, <strong>Dongyang Li<sup>✉</sup><strong>, and Puzhe Li<br>
-      <strong>MMM 2026 (CCF-C)</strong>
-      </li>
-      <li> <strong>LME-DETR: Lightweight and Multi-Scale Feature-Enhanced End-to-End Object Detection for Aerial Images</strong><br>
-      Feifei Xu, Yu Xie, <strong>Dongyang Li<sup>✉</sup><strong>, luobin Huang, Zhihao Guo<br>
-      <strong>MMM 2026</strong>
-      </li>
-      <li> <strong>CUOM: A Causal Unbiased Optimization Method for Federated Domain</strong><br>
-      Mi Wen, Kang Han, <strong>Dongyang Li</strong>, QiYe Cai, HaiLun Shen<br>
-      <strong>KBS (SCI Q1 journal)</strong> </p>
-      </li>
-      <li> <strong>Taming the Long Tail in Federated Learning: A Unified Global and Personalized Model Framework</strong><br>
-      Pengsong Zhang, Mi Wen, Zhou Zhu, <strong>Dongyang Li</strong><br>
-      <strong>Information Sciences (SCI Q2 journal)</strong> </p>
-      </li>
+      
       <ul>
       </div>
       `,
